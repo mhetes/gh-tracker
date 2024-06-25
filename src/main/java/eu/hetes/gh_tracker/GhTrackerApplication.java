ace770f65e082b3d44b05013cff6a1f4045645ff
Martin Heteš <martin@hetes.eu>
2024-06-25 22:42:05 +0200

    Initial commit from SpringInitializr

---
+++ src/main/java/eu/hetes/gh_tracker/GhTrackerApplication.java
@@ -0,0 +1,13 @@
+package eu.hetes.gh_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/hetes/gh_tracker/GhTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.hetes.gh_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
